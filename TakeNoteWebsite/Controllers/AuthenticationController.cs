ebf371c112d6a425f543603d8037d538574cbd2c
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2022-05-13 09:31:18 +0700

    fix dependency bugs

--- TakeNoteWebsite/Controllers/AuthenticationController.cs
+++ TakeNoteWebsite/Controllers/AuthenticationController.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TakeNoteWebsite.Data;
+using TakeNoteWebsite.Models.Data;
 
 namespace TakeNoteWebsite.Controllers
 {
