a8b8658a57cd45f2404d0f7927b848690a598a15
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2022-05-18 20:41:05 +0700

    add ImageFilter, change databaseQuery

---
+++ TakeNoteWebsite/Models/Data/ImageFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeNoteWebsite.Models.Data
+{
+    public class ImageFilter
+    {
+        public string Since { get; set; }
+        public string SortBy { get; set; }
+        public string Folder { get; set; }
+    }
+}
